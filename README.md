7f9727a698f34d674b80c4bcb0163507d6a90394
Korsah Solomon <korsahsolomon78@gmail.com>
2022-10-30 15:55:47 +0000

    Update 3 files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Portfoliowebsite
-Created with CodeSandbox
+# My Portfolio Website: A single page website  Built with react. 
+ # has 9 components 
+ # about,navbar, footer, experience, services, portfolio, contact,
+ # header and testimonials components
--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,3 +1,13 @@
 import "./navbar.css";
+import {AiOutlineHome} from "react-icons/ai";
 
-export default function Navbar() {}
+
+export default function Navbar() {
+    return(
+        <div>
+        <nav>
+            <a href="#"></AiOutlineHome/></a>
+        </nav>
+        </div>
+    )
+}
