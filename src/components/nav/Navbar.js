e3d86aaf2d38e4b4621cd4c29d7ee505ec3c5db0
Korsah Solomon <korsahsolomon78@gmail.com>
2022-10-30 15:58:08 +0000

    Update Navbar.js

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,13 +1,14 @@
 import "./navbar.css";
-import {AiOutlineHome} from "react-icons/ai";
-
+import { AiOutlineHome } from "react-icons/ai";
 
 export default function Navbar() {
-    return(
-        <div>
-        <nav>
-            <a href="#"></AiOutlineHome/></a>
-        </nav>
-        </div>
-    )
+  return (
+    <div>
+      <nav>
+        <a href="#">
+          <AiOutlineHome />
+        </a>
+      </nav>
+    </div>
+  );
 }
