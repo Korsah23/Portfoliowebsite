9d17b34c909709366c6c2fff972de6a5107d8dc2
Korsah23 <korsah.solomon31@gmail.com>
2022-11-22 13:50:15 +0000

    Finally Done, Removed backend

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,6 +1,7 @@
 import "./testimonials.css";
-import React, {useState,useEffect} from "react";
-import axios from "axios"
+import TestImg1 from "./TestImg1.jpg";
+import TestImg2 from "./TestImg2.jpg";
+import TestImg3 from "./TestImg3.jpg";
 
 // import Swiper core and required modules
 import { Pagination } from "swiper";
@@ -12,24 +13,7 @@ import "swiper/css"; //
 import "swiper/css/pagination" //pagination.min.css";
 
 export default function Testimonials() {
-  //set the state for getting testimonials 
- const [testimony,setTestimony] = useState([]);
- const count = 0;
- //make API request with axios
- useEffect(() => {
-  axios.get("https://portfoliobackend123.herokuapp.com/testimonials/",{
-    headers : {
-      'Authorization': 'Token b97873599f2afdd1cb4af80bafe884151b67df4a'
-    }
-  }).then((res)=>{
-    console.log(res.data)
-    setTestimony(res.data)
-  }
   
-  )
- }
- 
- ,[])
   
 
 
@@ -46,26 +30,42 @@ export default function Testimonials() {
         navigation
         pagination={{ clickable: true }}
       >
-        {
-          testimony.map((singleData) =>{
-            
-            return(
-              <SwiperSlide className="testimonial" key = {singleData.name}>
+         <SwiperSlide className="testimonial" >
               <div className="client__avatar">
-                <img src={singleData.image} alt="" />
+                <img src={TestImg1} alt="" />
               </div>
-              <h5 className="client__name">{singleData.name}</h5>
+              <h5 className="client__name">Lawrence Fesi</h5>
               <small className="client__review">
-                {singleData.message}
+              Excellent Work There. Didn't expect such on time delivery and clean and fast website design. 
+              I really love how he kept in touch with me all this while during the development of my website.
+               I recommend lawrence to everyone.
               </small>
             </SwiperSlide>
 
-            )
-            
-            
-          } ) 
-       
-}
+            <SwiperSlide className="testimonial">
+              <div className="client__avatar">
+                <img src={TestImg2} alt="" />
+              </div>
+              <h5 className="client__name">Mandy Ellis</h5>
+              <small className="client__review">
+              My company wanted someone to complete a simple web app development in few days.
+               I came across Solomon on Fiverr. 
+               I decided to outsource the work to him. Despite his cheap rate, he did the work as needed and we were impressed. 
+               Nice work there!
+              </small>
+            </SwiperSlide>
+
+            <SwiperSlide className="testimonial" >
+              <div className="client__avatar">
+                <img src={TestImg3} alt="" />
+              </div>
+              <h5 className="client__name">Clay Benson</h5>
+              <small className="client__review">
+              I first didn't realize the project will come as neat and well designed as it did. 
+              Solomon is a good developer who takes his time to get the job done.
+               I'm really Impressed! .Sorry I couldn't add my Picture though, this stock picture will do.
+              </small>
+            </SwiperSlide>
       </Swiper>
     </section>
   );
