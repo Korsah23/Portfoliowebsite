d04705c73bad6681367b45b19393d2a562d88af4
Korsah23 <korsah.solomon31@gmail.com>
2022-12-25 22:43:18 +0000

    Added portfolio items

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -2,6 +2,9 @@ import "./portfolio.css";
 import PortImg1 from "./PortImg1.jpg";
 import PortImg2 from "./PortImg2.png";
 import PortImg3 from "./UrlShort.jpg";
+import notes from "./notes.jpeg";
+import jodel from "./jodel.jpeg";
+import report from "./reportgen.jpeg";
 
 
 export default function Portfolio() {
@@ -69,6 +72,65 @@ export default function Portfolio() {
               </a>
             </div>
           </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__itemimage">
+              <img src={report} alt="" />
+            </div>
+            <h3>Report Generator(Django/Html/Bootstrap)</h3>
+            <div className="portfolio__itemcta">
+              <a href="https://github.com/Korsah23/reportgenerator" className="btn" target="_blank">
+                GitHub
+              </a>
+              <a
+                href="#"
+                className="btn btn-primary"
+                target="_blank"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+
+          <article className="portfolio__item">
+            <div className="portfolio__itemimage">
+              <img src={notes} alt="" />
+            </div>
+            <h3>Notes App(React/DjangoRest)</h3>
+            <div className="portfolio__itemcta">
+              <a href="https://github.com/Korsah23/simplenotesapp" className="btn" target="_blank">
+                GitHub
+              </a>
+              <a
+                href="#"
+                className="btn btn-primary"
+                target="_blank"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+
+
+          <article className="portfolio__item">
+            <div className="portfolio__itemimage">
+              <img src={jodel} alt="" />
+            </div>
+            <h3>JodelClone(Django/DjangoRest)</h3>
+            <div className="portfolio__itemcta">
+              <a href="https://github.com/Korsah23/anonymousmessagingapp/tree/master" className="btn" target="_blank">
+                GitHub
+              </a>
+              <a
+                href="#"
+                className="btn btn-primary"
+                target="_blank"
+              >
+                Live Demo
+              </a>
+            </div>
+          </article>
+
 
         
 
